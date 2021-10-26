8adef675f044558af718abe97389a5cafa302b6e
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-10-26 15:05:19 +0300

    initial commit, added some setup

---
+++ webpack.common.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  optimization: {
+    usedExports: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+      {
+        test: /\.(css|less)$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)/,
+        use: "file-loader",
+      },
+    ],
+  },
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    extensions: [".tsx", ".ts", ".js"],
+    fallback: {
+      fs: false,
+    },
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      filename: "index.html",
+      inject: "body",
+    }),
+    new FaviconsWebpackPlugin("./public/favicon.ico"),
+    new CleanWebpackPlugin(),
+    new Dotenv({
+      path: "./.env",
+    }),
+  ],
+};
