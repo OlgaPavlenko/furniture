5ad7ddab90ddfa2f864c630a1a6a475760c934fd
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-10-28 10:34:57 +0300

    rewrite Object.values to Object.keys for UserMenuItems

--- src/sharedComponents/Button/UserMenuItems/UserMenuItems.tsx
+++ src/sharedComponents/Button/UserMenuItems/UserMenuItems.tsx
@@ -2,11 +2,16 @@ import { FunctionComponent } from 'react';
 import { UserMenuItem } from 'sharedComponents/UserMenuItem/UserMenuItem';
 import { userMenuItems } from 'constants/constants';
 
+interface IUserMenuItems {
+  login: string;
+  cart: string;
+}
+
 export const UserMenuItems: FunctionComponent = () => {
   return (
     <div>
-      {Object.values(userMenuItems).map((item) => {
-        return <UserMenuItem key={item} src={item} />;
+      {Object.keys(userMenuItems as IUserMenuItems).map((key) => {
+        return <UserMenuItem key={key} src={userMenuItems[key as keyof IUserMenuItems]} />;
       })}
     </div>
   );
