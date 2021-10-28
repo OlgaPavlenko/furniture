8a8bd99b95a82ad18d5c22613feef39e4e7112df
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-10-28 10:11:36 +0300

    added some styles

--- src/sharedComponents/Footer/style.ts
+++ src/sharedComponents/Footer/style.ts
@@ -5,4 +5,27 @@ export const useStyle = makeStyles(() => ({
     backgroundColor: "#15166d",
     padding: "65px 25px 21px 25px",
   },
+
+  container: {
+    display: "flex",
+    flexWrap: "nowrap",
+    justifyContent: "space-around",
+    paddingBottom: "19px",
+    fontSize: "14px",
+  },
+
+  copyrite: {
+    paddingTop: "18px",
+    color: "rgb(212, 211, 211)",
+    borderTop: "1px solid rgba(255, 255, 255, 0.1)",
+    textAlign: "center",
+  },
+
+  footerContact: {
+    lineHeight: 1.71,
+  },
+
+  footerContactItem: {
+    marginBottom: "10px",
+  },
 }));
