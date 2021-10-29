7db734e426164e3e9ca2e10a38101814e6824934
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-10-29 17:20:11 +0300

    fixed non-serializable error

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import { productReducer } from './slices/product-slice';
-import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 const reducers = combineReducers({
   product: productReducer,
@@ -11,19 +10,7 @@ export const store = configureStore({
   reducer: reducers,
   middleware: (getDefaultMiddleware) => [
     ...getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [
-          'auth/registration/fulfilled',
-          'search/getMovieListWithQuery/pending',
-          'search/getMovieListWithQuery/fulfilled',
-          FLUSH,
-          REHYDRATE,
-          PAUSE,
-          PERSIST,
-          PURGE,
-          REGISTER,
-        ],
-      },
+      serializableCheck: false,
     }),
   ],
 });
