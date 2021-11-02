bbe3ecd994467310d59bd3e187659fb174ad0be7
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-11-02 17:01:04 +0200

    corrected input component

--- src/sharedComponents/Input/Input.tsx
+++ src/sharedComponents/Input/Input.tsx
@@ -1,9 +1,12 @@
-import { FunctionComponent } from 'react';
+import { DOMAttributes, FunctionComponent, SyntheticEvent } from 'react';
 
-interface IInput {
-  type: string;
+interface IInputProps extends DOMAttributes<HTMLElement> {
+  type?: string;
+  onClick?: (event: SyntheticEvent) => void;
+  shouldConcatHref?: boolean;
+  onChange?: (event: SyntheticEvent) => void;
 }
 
-export const Input: FunctionComponent<IInput> = ({ type }) => {
-  return <input type={type} />;
+export const Input: FunctionComponent<IInputProps> = ({ type, onClick }) => {
+  return <input type={type} onClick={onClick} />;
 };
