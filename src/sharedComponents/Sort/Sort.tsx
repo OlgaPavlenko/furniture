dd54c888d2f8c293f2fe26018b4920de318a1e70
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-11-17 12:57:15 +0200

    corrected sort order and if

--- src/sharedComponents/Sort/Sort.tsx
+++ src/sharedComponents/Sort/Sort.tsx
@@ -8,55 +8,47 @@ import { productListSelector } from 'store/selectors/product';
 import { IProduct } from 'utils/interfaces/product';
 import { setProductList } from 'store/slices/product';
 
-type Order = 'asc' | 'desc';
-
 export const Sort: FunctionComponent = () => {
   const classes = useStyle();
 
   const products = useSelector(productListSelector);
-  const [ascName, setAscName] = useState<Order>('asc');
-  const [ascPrice, setAscPrice] = useState<Order>('asc');
+  const [ascName, setAscName] = useState(1);
+  const [ascPrice, setAscPrice] = useState(1);
   const dispatch = useDispatch();
 
-  const reverOrder = (order: Order) => (order === 'asc' ? 'desc' : 'asc');
-
-  const sortBy = (type: keyof IProduct) => {
+  const sort = (type: keyof IProduct) => {
     const sortedProducts = [...products];
 
-    const sort = (order: Order) => {
-      const sortOrder = order === 'asc' ? 1 : -1;
-      sortedProducts.sort((prev: IProduct, next: IProduct) =>
-        prev[type] > next[type] ? -sortOrder : sortOrder,
-      );
-    };
+    const sortOrder = ascName < 0 || ascPrice < 0 ? -1 : 1;
+    const resultProducts = sortedProducts.sort((prev: IProduct, next: IProduct) =>
+      prev[type] > next[type] ? -sortOrder : sortOrder,
+    );
 
     if (type === 'price') {
-      sort(ascPrice);
-      setAscPrice(reverOrder(ascPrice));
+      setAscPrice(-ascPrice);
     } else {
-      sort(ascName);
-      setAscName(reverOrder(ascName));
+      setAscName(-ascName);
     }
 
-    dispatch(setProductList(sortedProducts));
+    dispatch(setProductList(resultProducts));
   };
 
   return (
     <div className="">
       <Button
         name="Price"
-        badgeSrc={ascPrice === 'asc' ? SORTING_IMGS.up : SORTING_IMGS.down}
+        badgeSrc={ascPrice === 1 ? SORTING_IMGS.up : SORTING_IMGS.down}
         className={classes.priceButton}
         onClick={() => {
-          sortBy('price');
+          sort('price');
         }}
       />
       <Button
         name="Name"
-        badgeSrc={ascName === 'asc' ? SORTING_IMGS.up : SORTING_IMGS.down}
+        badgeSrc={ascName === 1 ? SORTING_IMGS.up : SORTING_IMGS.down}
         className="buttons"
         onClick={() => {
-          sortBy('name');
+          sort('name');
         }}
       />
     </div>
