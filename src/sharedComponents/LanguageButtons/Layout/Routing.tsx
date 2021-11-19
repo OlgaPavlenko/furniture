07953ddd1eeab8f55cd3a28d99b365096077b668
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-11-19 16:17:02 +0200

    added missed pages

--- src/sharedComponents/LanguageButtons/Layout/Routing.tsx
+++ src/sharedComponents/LanguageButtons/Layout/Routing.tsx
@@ -1,5 +1,6 @@
 import { AboutUs } from 'pages/AboutUs/AboutUs';
 import { Cart } from 'pages/Cart/Cart';
+import { Delivery } from 'pages/Delivery/Delivery';
 import { ProductCardList } from 'pages/ProductCardList';
 import { ProductDetailView } from 'pages/ProductDetailView/ProductDetailView';
 import { Switch, Redirect, Route } from 'react-router-dom';
@@ -9,8 +10,9 @@ export function Routing() {
     <Switch>
       <Redirect exact from="/" to="/catalog" />
       <Route path="/products/:id" component={ProductDetailView} />
-      <Route path="/catalog" component={ProductCardList } />
+      <Route path="/catalog" component={ProductCardList} />
       <Route path="/aboutUs" component={AboutUs} />
+      <Route path="/delivery" component={Delivery} />
       <Route path="/cart" component={Cart} />
     </Switch>
   );
