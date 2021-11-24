1d2bfac7eb1f0bfef045fbc7fb96eb108269b186
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-11-24 11:38:11 +0200

    added persist-redux and fixed page-reload

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'store/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { store } from './store/store';
-
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { filterReducer } from './slices/filter';
 import { productReducer } from './slices/product';
@@ -9,16 +11,26 @@ export interface IRootState {
   filter: ReturnType<typeof filterReducer>;
 }
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['product'],
+};
+
 const reducers = combineReducers({
   product: productReducer,
   filter: filterReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: reducers,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => [
     ...getDefaultMiddleware({
       serializableCheck: false,
     }),
   ],
 });
+
+export const persistor = persistStore(store);
